00b8f7668d0b12a1b831d1f45ab854b6e7e62c09
Rzhvms <165849129+Rzhvms@users.noreply.github.com>
2025-03-16 22:03:53 +0500

    Add files via upload

---
+++ MethodsOfTaskSolution/Task_E_SplitArray/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Task_E_SplitArray
+{
+    public class SplitArray
+    {
+        static void Main()
+        {
+            var inputValues = Console.ReadLine().Split(' ');
+            var n = int.Parse(inputValues[0]);
+            var k = int.Parse(inputValues[1]);
+
+            var array = Console.ReadLine().Split(' ');
+            var intArray = Array.ConvertAll(array, int.Parse);
+
+            var result = GetValue(n, k, intArray);
+
+            Console.WriteLine(result);
+        }
+
+        static long GetValue(int n, int k, int[] intArray)
+        {
+            long sum = 0;
+            long maxValue = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += intArray[i];
+                if (intArray[i] > maxValue)
+                {
+                    maxValue = intArray[i];
+                }
+            }
+
+            long left = maxValue;
+            long right = sum;
+
+            while (left <= right)
+            {
+                long mid = (left + right) / 2;
+                long count = 1;
+                long currentSum = intArray[0];
+
+                for (int i = 1; i < intArray.Length; i++)
+                {
+                    if (currentSum + intArray[i] <= mid)
+                    {
+                        currentSum += intArray[i];
+                    }
+                    else
+                    {
+                        currentSum = intArray[i];
+                        count++;
+                    }
+                }
+
+                if (count <= k)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+
+            return left;
+        }
+    }
+}
