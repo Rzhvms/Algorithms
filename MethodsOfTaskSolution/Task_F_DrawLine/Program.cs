66ce7dfc2b64c3257f537d62066869524d48a9aa
Rzhvms <165849129+Rzhvms@users.noreply.github.com>
2025-03-18 13:11:38 +0500

    Add files via upload

---
+++ MethodsOfTaskSolution/Task_F_DrawLine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_F_DrawLine
+{
+    class Task_F_DrawLine
+    {
+        static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            List<(int time, int type)> array = new List<(int, int)>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                var left = int.Parse(input[0]);
+                var right = int.Parse(input[1]);
+
+                array.Add((left, 1));
+                array.Add((right, -1));
+            }
+
+            array.Sort();
+
+            var counter = 1;
+            var result = 0;
+
+            for (int i = 1; i < array.Count; i++)
+            {
+                if (counter != 0)
+                {
+                    result += (array[i].time - array[i-1].time);
+                }
+                counter += array[i].type;
+            }
+            
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MethodsOfTaskSolution/Task_G_TicketWindows/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_G_TicketWindows
+{
+    class TicketWindows
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var events = new List<(int time, int type)>();
+            int allWindows = n;
+            int curWindows = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split(' ');
+                var nums = Array.ConvertAll(input, int.Parse);
+
+                int startTime = ConvertToSeconds(nums[0], nums[1], nums[2]);
+                int endTime = ConvertToSeconds(nums[3], nums[4], nums[5]);
+
+                if (startTime == endTime)
+                {
+                    allWindows--;
+                }
+                else
+                {
+                    events.Add((startTime, 1));
+                    events.Add((endTime, -1));
+                    if (startTime > endTime)
+                    {
+                        curWindows++;
+                    }
+                }
+            }
+
+            events.Sort();
+            int result = CalculateTotalTime(events, curWindows, allWindows);
+
+            Console.WriteLine(result);
+        }
+
+        static int ConvertToSeconds(int hours, int minutes, int seconds)
+        {
+            return hours * 3600 + minutes * 60 + seconds;
+        }
+
+        static int CalculateTotalTime(List<(int time, int type)> events, int curWindows, int allWindows)
+        {
+            int result = 0;
+
+            if (curWindows == allWindows)
+            {
+                result += events[0].time;
+            }
+
+            for (int i = 0; i < events.Count; i++)
+            {
+                curWindows += events[i].type;
+
+                if (curWindows == allWindows)
+                {
+                    if (i != events.Count - 1)
+                    {
+                        result += events[i + 1].time - events[i].time;
+                    }
+                    else
+                    {
+                        result += 24 * 3600 - events[i].time;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
